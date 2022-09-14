0ecad93614b5c33f52a58c473523d2e69173ec1f
Kanit <kanit.je@rmuti.ac.th>
2022-09-14 13:01:21 +0700

    update image with swal2

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Illuminate\Console\View\Components\Alert as ComponentsAlert;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use RealRashid\SweetAlert\Facades\Alert;
 
 use function PHPSTORM_META\type;
 
@@ -66,16 +68,20 @@ class ArticleController extends Controller
         //     'views' => $request->views,
         // ]);
 
-        if ($image = $request->file('image')) {
+        if ($request->file('image') != null) {
             // $path = 'storage/images';
             // $replace_path = str_replace("public", "storage", $path);
             // $profileImage = date('YmdHis') . "." . $image->getClientOriginalExtension();
             // $image->move($path, $profileImage);
             $profileImage = Storage::putFile('public/images', $request->file('image'));
             $input['image'] = basename($profileImage);
-        }
+        } else {
+            $input['image'] = '';
+        };
 
-        Article::create($input);
+        if (Article::create($input)) {
+            Alert::success('แจ้งเตือน', 'เพิ่มข้อมูลสำเร็จ');
+        }
 
         return redirect()->route('article.index');
     }
@@ -111,18 +117,19 @@ class ArticleController extends Controller
     public function update(Request $request, Article $article)
     {
         $validatedData = $request->validate([
-            'image'=>'required',
             'type' => 'required',
             'title' => 'required',
             'detail' => 'required',
         ]);
 
-        if ($image = $request->file('image')) {
+        if ($image = $request->file('image') != null) {
             $image = Storage::putFile('public/images', $request->file('image'));
             $save_image =  $article->image = basename($image);
         } else {
             $save_image = '';
         }
+
+        Alert::Success('แจ้งเตือน!', 'แก้ไขข้อมูลสำเร็จ');
 
         // $path = $request->image->store('public/photos');
         // $replace_path = str_replace("public", "storage", $path);
@@ -144,7 +151,17 @@ class ArticleController extends Controller
      */
     public function destroy(Article $article)
     {
-        $article->delete();
+       
+
+        if ($article->image != null) {
+             $image_path = 'storage/images/' . $article->image;
+            unlink($image_path);
+            $article->delete();
+        } else {
+            $article->delete();
+        };
+
+        Alert::success('แจ้งเตือน', 'ลบข้อมูลสำเร็จ');
         return redirect()->route('article.index');
     }
 }
